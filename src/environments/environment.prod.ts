04dc33fac182b7f5fc98aad4c67c1e1fed78794c
JuanDavidRicardo <juan.d.ricardo.t@gmail.com>
2020-08-21 20:57:23 -0500

    Cambio de url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  UrlBaseApi:'http://localhost:1337/'
-  /*UrlBaseApi:'https://strapi-onlineapp.herokuapp.com/'*/  
+   //UrlBaseApi:'http://localhost:1337/'
+   UrlBaseApi:'https://app-apiteleconsulta.herokuapp.com/'  
 };

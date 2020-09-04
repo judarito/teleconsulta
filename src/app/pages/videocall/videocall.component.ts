a8fa4497e7cf0bb24e55c5a3d0a1537bce153c52
JuanDavidRicardo <juan.d.ricardo.t@gmail.com>
2020-09-03 21:22:05 -0500

    Modificaciones generales

--- src/app/pages/videocall/videocall.component.ts
+++ src/app/pages/videocall/videocall.component.ts
@@ -9,11 +9,13 @@ import { ActivatedRoute } from '@angular/router';
 export class VideocallComponent implements OnInit {
 
   public RoomId:string;
+  public urlTokboxIframe:string="";
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
    this.route.params.subscribe(params => {
       this.RoomId=params.id
+      this.urlTokboxIframe=`https://tokbox.com/embed/embed/ot-embed.js?embedId=3b0e2358-bf4c-4738-a2c6-f8e9bbd1c776&room=${this.RoomId}&iframe=true`
    });
   }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
    //UrlBaseApi:'http://localhost:1337/'
-   UrlBaseApi:'http://18.222.231.56:3000/'  
+   UrlBaseApi:'https://api.sysconsulta.com/'  
 };
